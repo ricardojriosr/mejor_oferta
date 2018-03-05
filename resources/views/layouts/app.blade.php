b8dab3585e14a69db5acfa95c204f46d24d5deb1
Ricardo Rios <ricardojriosr@gmail.com>
2018-03-05 12:23:44 -0500

    Layouts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,17 +97,17 @@
         @yield('content')
     </div>
 
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
+
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="/js/app.js"></script>
     <script src="/js/fileinput.min.js"></script>
     <script>
     $(function() {
         $("body").on("click", ".delete-button", function() {
             var r = confirm("Are You Sure to Delete This Record?");
-            return r;
+            return r;En
         });
     });
     </script>
